6bb91c1730a2ecad528a748edd337bf518751e81
Bruno Medeiros <brunos.hgm@gmail.com>
2023-05-23 21:11:13 -0300

    tela de agradecimentgo e envio do formulario

--- sendEmail.php
+++ sendEmail.php
@@ -3,26 +3,27 @@
 if(isset($_POST['enviar'])) {
 
     // Defina o destinatário e o remetente
-    $to = "seuemail@seudominio.com";
+//    $to = "hadsondesigner@gmail.com";
+    $destino = "brunos.hgm@gmail.com";
     $from = $_POST['email'];
 
     // Defina o assunto
-    $subject = "Novo formulário enviado do site";
+    $assunto = "Novo formulário do site";
 
     // Defina o corpo da mensagem
-    $message = "Nome: " . $_POST['nome'] . "\r\n";
-    $message .= "Email: " . $_POST['email'] . "\r\n";
-    $message .= "WhatsApp: " . $_POST['whatsapp'] . "\r\n";
-    $message .= "Empresa: " . $_POST['empresa'] . "\r\n";
-    $message .= "Cidade: " . $_POST['cidade'] . "\r\n";
-    $message .= "Estado: " . $_POST['estado'] . "\r\n";
-    $message .= "Site: " . $_POST['site'] . "\r\n";
-    $message .= "Instagram: " . $_POST['instagram'] . "\r\n";
-    $message .= "Como me encontrou?: " . $_POST['encontrou'] . "\r\n";
-    $message .= "Por que precisa desse projeto? O que espera?: " . $_POST['projeto'] . "\r\n";
-    $message .= "O que sua empresa faz/vende?: " . $_POST['categoria'] . "\r\n";
-    $message .= "Tamanho da empresa: " . $_POST['tamanho'] . "\r\n";
-    $message .= "Itens necessários: " . implode(", ", $_POST['itens']) . "\r\n";
+    $menssagem = "Nome: " . $_POST['nome'] . "\r\n";
+    $menssagem .= "Email: " . $_POST['email'] . "\r\n";
+    $menssagem .= "WhatsApp: " . $_POST['whatsapp'] . "\r\n";
+    $menssagem .= "Empresa: " . $_POST['empresa'] . "\r\n";
+    $menssagem .= "Cidade: " . $_POST['cidade'] . "\r\n";
+    $menssagem .= "Estado: " . $_POST['estado'] . "\r\n";
+    $menssagem .= "Site: " . $_POST['site'] . "\r\n";
+    $menssagem .= "Instagram: " . $_POST['instagram'] . "\r\n";
+    $menssagem .= "Como me encontrou?: " . $_POST['encontrou'] . "\r\n";
+    $menssagem .= "Por que precisa desse projeto? O que espera?: " . $_POST['projeto'] . "\r\n";
+    $menssagem .= "O que sua empresa faz/vende?: " . $_POST['categoria'] . "\r\n";
+    $menssagem .= "Tamanho da empresa: " . $_POST['tamanho'] . "\r\n";
+    $menssagem .= "Itens necessários: " . implode(", ", $_POST['itens']) . "\r\n";
 
     // Defina os cabeçalhos da mensagem
     $headers = "From: $from \r\n";
@@ -32,10 +33,19 @@ if(isset($_POST['enviar'])) {
     $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
 
     // Envie o email
-    mail($to, $subject, $message, $headers);
+    $enviaremail = mail($destino, $assunto, $menssagem, $headers);
 
     // Exiba uma mensagem de sucesso
     echo "<p>Obrigado por entrar em contato!</p>";
+
+
+    if($enviaremail){
+        $mgm = "E-MAIL ENVIADO COM SUCESSO! <br> O link será enviado para o e-mail fornecido no formulário";
+        echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+    } else {
+        $mgm = "ERRO AO ENVIAR E-MAIL!";
+        echo "";
+    }
 
 }
 ?>
