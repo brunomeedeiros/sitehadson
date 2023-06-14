61988cc0f71de585f55ceb5ef19d6e02ea9081f5
Bruno Medeiros <brunos.hgm@gmail.com>
2023-06-14 20:34:49 -0300

    Melhoria nas validações dos campos do formulário.

--- formulario-controller.js
+++ formulario-controller.js
@@ -7,7 +7,20 @@ const cidadeInput = document.getElementById('cidade');
 const estadoInput = document.getElementById('estado');
 const submitButton = document.getElementById('submit-button');
 const radioComoEncontrou = document.querySelectorAll('input[name="encontrou"]');
-var radioComoEncontrouMarcado = false;
+let radioComoEncontrouMarcado = false;
+let projetoInput = document.getElementById('projeto');
+let projetoError = document.getElementById('projeto-error');
+let projetoValue = projetoInput.value;
+let radioEmpresaFazVende = document.querySelectorAll('input[name="categoria"]');
+let radioEmpresaFazVendeMarcado = false;
+let categoriaError = document.getElementById('categoria-error');
+let radioTamanhoEmpresa = document.querySelectorAll('input[name="empresa"]');
+let radioTamanhoEmpresaMarcado = false;
+let empresaError = document.getElementById('empresa-error');
+let checkItens = document.querySelectorAll('input[name="itens[]"]');
+let checkItensMarcado = false;
+let itensError = document.getElementById('itens-error');
+
 submitButton.addEventListener('click', function(event) {
     let isValid = true;
 
@@ -44,7 +57,7 @@ submitButton.addEventListener('click', function(event) {
         isValid = false;
     }
 
-    for (var i = 0; i < radioComoEncontrou.length; i++) {
+    for (let i = 0; i < radioComoEncontrou.length; i++) {
         if (radioComoEncontrou[i].checked) {
             radioComoEncontrouMarcado = true;
             break;
@@ -54,6 +67,57 @@ submitButton.addEventListener('click', function(event) {
     if (!radioComoEncontrouMarcado) {
         event.preventDefault();
         alert('Por favor, selecione como você nos encontrou.');
+    }
+
+    if (projetoValue === '') {
+        event.preventDefault();
+        projetoInput.classList.add('error');
+        projetoError.textContent = 'Por favor, preencha este campo.';
+    } else {
+        projetoInput.classList.remove('error');
+        projetoError.textContent = '';
+    }
+
+    for (let i = 0; i < radioEmpresaFazVende.length; i++) {
+        if (radioEmpresaFazVende[i].checked) {
+            radioEmpresaFazVendeMarcado = true;
+            break;
+        }
+    }
+
+    if (!radioEmpresaFazVendeMarcado) {
+        event.preventDefault();
+        categoriaError.textContent = 'Por favor, selecione uma categoria.';
+    } else {
+        categoriaError.textContent = '';
+    }
+
+    for (let i = 0; i < radioTamanhoEmpresa.length; i++) {
+        if (radioTamanhoEmpresa[i].checked) {
+            radioTamanhoEmpresaMarcado = true;
+            break;
+        }
+    }
+
+    if (!radioTamanhoEmpresaMarcado) {
+        event.preventDefault();
+        empresaError.textContent = 'Por favor, selecione o porte da empresa.';
+    } else {
+        empresaError.textContent = '';
+    }
+
+    for (let i = 0; i < checkItens.length; i++) {
+        if (checkItens[i].checked) {
+            checkItensMarcado = true;
+            break;
+        }
+    }
+
+    if (!checkItensMarcado) {
+        event.preventDefault();
+        itensError.textContent = 'Por favor, selecione pelo menos um item.';
+    } else {
+        itensError.textContent = '';
     }
 
     if (!isValid) {
